e270d60def417ad7d395e5cc9d24b0929df384fc
GabrielHR0sa <rosahenriquegabriel@gmail.com>
2023-10-15 22:40:28 -0300

    Componentizing Widgets

--- flutter_project1/hello_world/lib/main.dart
+++ flutter_project1/hello_world/lib/main.dart
@@ -1,35 +1,7 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:hello_world/myapp.dart';
 
 void main() {
   runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Home')),
-      body: Center(
-        child: Text('Hello World'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          print('Clicou');
-        },
-      ),
-    );
-  }
 }
