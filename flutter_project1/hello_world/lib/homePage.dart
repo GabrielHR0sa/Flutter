07e37bbdece792a02096c8796931413eaefeac8e
GabrielHR0sa <rosahenriquegabriel@gmail.com>
2023-10-15 23:07:17 -0300

    InheritedWidget

--- flutter_project1/hello_world/lib/homePage.dart
+++ flutter_project1/hello_world/lib/homePage.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/homeController.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var controller = HomeController.of(context);
+
     return Scaffold(
       appBar: AppBar(title: Text('Home')),
       body: Center(
-        child: Text('Hello World'),
+        child: Text('Contador ${controller.value}'),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
-          print('Clicou');
+          controller.increment();
         },
       ),
     );
--- flutter_project1/hello_world/lib/myApp.dart
+++ flutter_project1/hello_world/lib/myApp.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/homeController.dart';
 import 'package:hello_world/homePage.dart';
 import 'package:hello_world/main.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
+    return MaterialApp(
+      home: HomeController(
+        child: HomePage(),
+      ),
     );
   }
 }
