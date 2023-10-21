88beab0557be055abaf9e031f35dad253ad0cde6
GabrielHR0sa <rosahenriquegabriel@gmail.com>
2023-10-21 16:22:33 -0300

    Adding column and row materials

---
+++ column_row/lib/column.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //debugShowCheckedModeBanner: false, // retirar o banner de debug
+      home: HomePage(),
+    );
+  }
+}
+
+var counter = 1;
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      width: MediaQuery.of(context).size.width,
+      child: Builder(builder: (context) {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround, //eixo y
+          crossAxisAlignment: CrossAxisAlignment.center, //eixo x
+          children: [
+            Container(
+              color: Colors.red,
+              height: 100,
+              width: 100,
+            ),
+            Container(
+              color: Color.fromARGB(255, 0, 17, 255),
+              height: 100,
+              width: 100,
+            ),
+            Container(
+              color: Color.fromARGB(255, 169, 23, 236),
+              height: 100,
+              width: 100,
+            ),
+            Container(
+              color: Color.fromARGB(255, 0, 0, 0),
+              height: 100,
+              width: 100,
+            ),
+          ],
+        );
+      }),
+    ));
+  }
+}
